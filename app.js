ad92f4670afdd3a0f60c181065a7cd803840d0e5
Cassius <cassius.reynolds.dev@gmail.com>
2024-05-23 13:47:52 -0700

    trying new audio paths again to fix issue, this time with the name of the root first in an absolute path

--- app.js
+++ app.js
@@ -18,10 +18,12 @@ let finalGrid = [];
 let lastRowStart = gridSize * gridSize - gridSize;
 let lastRowStop = gridSize * gridSize - 1;
 // audio vars
-const gridPressSoundPath = "/public/audio/grid-press.wav";
-const gridFlashSoundPath = "/public/audio/grid-flash.wav";
-const levelPassAudio = new Audio("/public/audio/level-pass-audio.mp3");
-const gameoverAudio = new Audio("/public/audio/game-over-buzzer.wav");
+// audio vars
+const gridPressSoundPath = "/memory-squares/public/audio/grid-press.wav";
+const gridFlashSoundPath = "/memory-squares/public/audio/grid-flash.wav";
+const levelPassAudio = new Audio("/memory-squares/public/audio/level-pass-audio.mp3");
+const gameoverAudio = new Audio("/memory-squares/public/audio/game-over-buzzer.wav");
+
 
 
 // ending game
